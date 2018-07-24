e3fb5b701e4e58925ec43ba42b22c4267990c454
Orlando Romo <orlando.romo.97@hotmail.com>
2018-07-24 02:31:04 -0500

    Titulo a la app, validacion por si se empata, David es pasivo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
+import { AboutPage } from './../pages/about/about';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
+// import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage:any = AboutPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -10,55 +10,63 @@ export class AboutPage {
   public imgSrc: String = '';
   //Tablero
   public board: any[] = [];
+  // Variable que comprueba que se llenó el tablero
+  public filled: boolean = false;
   //Número de la columna de dónde se quiere dejar caer la ficha
   public selectedGhost: number = 4;
   //Varable para saber el turno
   public P1Turn: boolean = true;
   //Variable que se usa para no permitir ningún moviemiento cuando una ficha este cayendo
-  protected isWorking: boolean = false; 
+  protected isWorking: boolean = false;
   //Profundidad de la columna
-  public columnLevel : number[]  = [0,0,0,0,0,0,0];
+  public columnLevel: number[] = [0, 0, 0, 0, 0, 0, 0];
   //Número de espacios llenos en el tablero
-  private fullSpaces : number = 0;
+  private fullSpaces: number = 0;
 
   //URL de las imágenes de las fichas
   protected P1IMGSRC = 'https://vignette.wikia.nocookie.net/rickandmorty/images/b/b5/Morty_Token_largec.png/revision/latest?cb=20161001191020';
   protected P2IMGSRC = 'https://vignette.wikia.nocookie.net/rickandmorty/images/7/75/Blips_and_Chitz_Coupon.png/revision/latest?cb=20160909050613'
 
   constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
-     this.imgSrc = this.P1IMGSRC;
-     //Se rellena el tablero con una imagen en blanco
-     for (var i=0; i<6; i++) {
+
+    this.imgSrc = this.P1IMGSRC;
+    //Se rellena el tablero con una imagen en blanco
+    for (var i = 0; i < 6; i++) {
       let array: any[] = [];
-       for (var j=0; j<7; j++) {
-         array.push({
-           selectedBy: null,
-           img: 'https://vignette.wikia.nocookie.net/nikita2010/images/d/d2/Blank.png/revision/latest?cb=20130725195235'
-         });
-       }
-       this.board.push({array: array});
-     }
+      for (var j = 0; j < 7; j++) {
+        array.push({
+          selectedBy: null,
+          img: 'https://vignette.wikia.nocookie.net/nikita2010/images/d/d2/Blank.png/revision/latest?cb=20130725195235'
+        });
+      }
+      this.board.push({ array: array });
+    }
   }
 
   //Función que se llama cuando se presiona click, hace que la ficha baje en la columna seleccionada
   takeTurn() {
     if (!this.isWorking) {
       let ghost = this.selectedGhost; //columna seleccionada (1-7)
-      let selectedColumnLevel = this.columnLevel[ghost-1]+1;
+      let selectedColumnLevel = this.columnLevel[ghost - 1] + 1;
       if (selectedColumnLevel < 7) {
         this.playSound();
         this.isWorking = true;
         document.querySelector(`#ghost${ghost}`).classList.add(`animated-${selectedColumnLevel}`);
-        setTimeout( () => {
-          this.columnLevel[ghost-1] = selectedColumnLevel;
+        setTimeout(() => {
+          this.columnLevel[ghost - 1] = selectedColumnLevel;
 
           //Se le asignan valores a las propiedades del campo del tablero
-          if(this.P1Turn) {
-            this.board[6-selectedColumnLevel].array[ghost-1].img = this.P1IMGSRC;
-            this.board[6-selectedColumnLevel].array[ghost-1].selectedBy = 'P1';
+          // console.log('Columna ', this.selectedGhost);
+          // console.log('Fila ', selectedColumnLevel);
+          // console.log('Nivel de columna ', this.columnLevel);
+
+
+          if (this.P1Turn) {
+            this.board[6 - selectedColumnLevel].array[ghost - 1].img = this.P1IMGSRC;
+            this.board[6 - selectedColumnLevel].array[ghost - 1].selectedBy = 'P1';
           } else {
-            this.board[6-selectedColumnLevel].array[ghost-1].img = this.P2IMGSRC;
-            this.board[6-selectedColumnLevel].array[ghost-1].selectedBy = 'P2';
+            this.board[6 - selectedColumnLevel].array[ghost - 1].img = this.P2IMGSRC;
+            this.board[6 - selectedColumnLevel].array[ghost - 1].selectedBy = 'P2';
           }
 
           //Se cambia el turno (variables, imágenes)
@@ -76,8 +84,11 @@ export class AboutPage {
           if (selectedColumnLevel == 6) {
             document.querySelector(`#ghost${ghost}`).classList.add('full-column');
           }
+          if (this.checkAllBoard()) {
+            this.Tie();
+          }
           this.fullSpaces++;
-          console.log(this.fullSpaces);
+          // console.log('espacios we ', this.fullSpaces);
         }, 1000);
       }
     }
@@ -85,14 +96,19 @@ export class AboutPage {
 
   //Alerta de Empate *aún no se usa*
   Tie() {
-    if (!this.isWorking) {
-      let alert = this.alertCtrl.create({
-        title: 'Miss',
-        subTitle: 'It\'s a tie!',
-        buttons: ['Dismiss']
-      });
-      alert.present();
-    } 
+    // if (!this.isWorking) {
+    // }
+    let alert = this.alertCtrl.create({
+      title: 'Miss',
+      subTitle: 'It\'s a tie!',
+      buttons: [{
+        text:'Dismiss',
+        handler:()=>{
+          this.reiniciar();
+        }
+      }]
+    });
+    alert.present();
   }
 
   //Cuando el usuario mueve el dedo hacia la derecha
@@ -103,7 +119,7 @@ export class AboutPage {
       }
     }
   }
-  
+
   //Cuando el usuario mueve el dedo hacia la derecha
   moveLeft() {
     if (!this.isWorking) {
@@ -111,15 +127,29 @@ export class AboutPage {
         this.selectedGhost--;
       }
     }
-   }
+  }
 
-   playSound() {
+  playSound() {
     let audio = new Audio();
     audio.src = '/assets/sounds/tokenSound.mp3';
     audio.play();
-   }
+  }
 
-   reiniciar() {
+  reiniciar() {
     this.navCtrl.setRoot(this.navCtrl.getActive().component);
-   }
+  }
+  checkAllBoard() {
+    let counter: number = 0;
+    let showtoast: boolean = false;
+    this.columnLevel.forEach((element: number) => {
+
+      if (element === 6)
+        counter++;
+      if (counter === 7) {
+        showtoast = true;
+      }
+    });
+    console.log('contador ', counter);
+    return showtoast;
+  }
 }
